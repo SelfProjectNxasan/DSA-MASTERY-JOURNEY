0b3b8f321559df47fc53d8f15f4d32cf9dbe0fbd
Unathi Nxasana <UnathiN@hollywoodbets.net>
2025-11-29 00:20:58 +0200

    Proceed Tomorrow

--- arrays.py
+++ arrays.py
@@ -1,6 +1,29 @@
-
-numbers = [1,1,3,4,5]
-sum_nums = 0
-for j in range(0,len(numbers)):
-    sum_nums += numbers[j]
-print(f" SUM OF DIGITS [{sum_nums}]")
+def findTarget(array_input: list, target):
+    isFound = False
+    if type(target) != type(array_input[0]):
+        print(f"TRAGET TYPE IS {type(target)}  WHEILE ELEMENT IS {type(array_input[0])}")
+        return False
+    for k in range(0,len(array_input)):
+        if(array_input[k] == target):
+            isFound = True
+            break
+    return isFound
+def Replace(input_array:list,old_val,new_val)->list:
+    for i in range(0,len(input_array)):
+        if(input_array[i] == old_val):
+            input_array[i] = new_val
+    return input_array
+def AddItem(ar:list,item_add)->list:
+    ar.append(item_add)
+    return ar
+def RemoveItem(a:list,item)->list:
+    ret = []
+    for i in range(0,len(a)):
+        if a[i] != item:
+            ret.append(a[i])
+    return ret
+def get_odd(ar:list)->int:
+    return [x for x in ar if x % 2 != 0]
+elements = [57,8,9,4,8,4,8,44,3]
+is_five = get_odd(elements)
+print(f"ODD NUMBERS = [{is_five}]")
